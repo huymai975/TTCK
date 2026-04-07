74672ffb0be864ca4690558343f1968389e45395
huymai975 <huymai975@gmail.com>
2026-04-08 02:20:43 +0700

    init commit

---
+++ WebAppBookingBoat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppBookingBoat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
