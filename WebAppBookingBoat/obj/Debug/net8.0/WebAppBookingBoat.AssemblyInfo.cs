ab248d4b53274fa488f6bd747cee1e058e2fb4b4
huymai975 <huymai975@gmail.com>
2026-04-08 20:33:33 +0700

    areas cho admin

--- WebAppBookingBoat/obj/Debug/net8.0/WebAppBookingBoat.AssemblyInfo.cs
+++ WebAppBookingBoat/obj/Debug/net8.0/WebAppBookingBoat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppBookingBoat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bec100a091a0d3845ad040cb282361cd331534cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40cbf79453af6160df7a82ae4033212d02df59d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppBookingBoat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppBookingBoat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
